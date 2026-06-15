dd1eaf57a5a1599b15067d3e0112e91f498b8d7a
C.C <linyuan213@gmail.com>
2026-06-15 06:28:12 +0000

    fix(other): 修复 ESLint 配置并添加 ghcr 推送权限

--- internal/lint-configs/eslint-config/src/configs/unicorn.ts
+++ internal/lint-configs/eslint-config/src/configs/unicorn.ts
@@ -35,7 +35,7 @@ export async function unicorn(): Promise<Linter.Config[]> {
         'unicorn/no-useless-undefined': 'off',
         'unicorn/prefer-at': 'off',
         'unicorn/prefer-dom-node-text-content': 'off',
-        'unicorn/prefer-export-from': ['error', { ignoreUsedVariables: true }],
+        'unicorn/prefer-export-from': 'error',
         'unicorn/prefer-top-level-await': 'off',
         'unicorn/prevent-abbreviations': 'off',
       },
